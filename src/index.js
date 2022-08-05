c07aa148862ac40da24d7745656544adc1f118ec
Gingaling <nycgingsf@hotmail.com>
2022-08-04 21:58:05 -0700

    Changed readme

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    redirectUri={window.location.origin}>
+    redirectUri={https://resilient-kashata-b34c57.netlify.app}>
     <App />
-    </Auth0Provider>
+  </Auth0Provider>
 );
